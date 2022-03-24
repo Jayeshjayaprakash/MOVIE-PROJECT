c0f3a2e2014345aef9f975327b65b8ebddfb50d2
Jayeshjayaprakash <98200798+Jayeshjayaprakash@users.noreply.github.com>
2022-03-24 16:35:51 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<router-view />
+</template>
+
+<style lang="scss">
+	body {
+		background-color: #222b31;
+		margin: 0;
+	}
+
+	.m-0 {
+		margin: 0;
+	}
+
+	#app {
+  font-family: -apple-system,BlinkMacSystemFont,
+  "Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,
+  "Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #fdfdfd;
+}
+
+	.card-shadow {
+		-webkit-box-shadow: 0px 5px 5px 0px rgba(8, 8, 8, 0.5);
+		-moz-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.5);
+		box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.5);
+	}
+</style>
